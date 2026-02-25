0439f33a512703d43b6bd43e72df1466aaa70d0d
Sirenion <gizmode338@gmail.com>
2026-02-25 14:33:05 +0300

    SeasonsResponse type fix

--- lib/data/model/search/search_seasons_model.dart
+++ lib/data/model/search/search_seasons_model.dart
@@ -5,8 +5,8 @@ abstract class SeasonsResponseModel with _$SeasonsResponseModel {
   const factory SeasonsResponseModel({
     required List<SeasonsModel> docs,
     required int limit,
-    required dynamic next,
-    required dynamic prev,
+    required String next,
+    required String prev,
     required bool hasNext,
     required bool hasPrev,
   }) = _SeasonsResponseModel;
